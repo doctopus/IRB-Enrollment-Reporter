ac7435c968fcc035076cb681e673de2377baad10
Doctopus <milan.najim@gmail.com>
2023-07-07 09:23:13 -0400

    Pandas, Numpy, Matplotlib

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# IRB-Enrollment-Reporter
+# IRB Enrollment Reporter
+
+### Fetch new patients enrolled in the IRB  
+### Analyze the data and report it in a PDF
+
+This project uses poetry virtualenv and python 3.11
--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sqlalchemy import create_engine
+# Replace 'username', 'password', 'host', 'port', and 'database_name' with your actual database credentials
+engine = create_engine('mysql+pymysql://username:password@host:port/database_name')
+connection = engine.connect()
+result = connection.execute('SELECT * FROM your_table')
+for row in result:
+    print(row)
+
+connection.close()
+
 excel_data = "data/enrollment.xlsx"
 
 df_excel =pd.read_excel(excel_data, sheet_name='Sheet1')
-print(df_excel)
+print(df_excel['MRN'])
